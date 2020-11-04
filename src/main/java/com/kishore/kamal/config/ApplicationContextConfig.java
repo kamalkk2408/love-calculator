6d41fb93a536666e022594f29531760bbcaf4612
kamal <k0k03lc@homeoffice.wal-mart.com>
2020-11-04 14:41:16 +0530

    adding

--- src/main/java/com/kishore/kamal/config/ApplicationContextConfig.java
+++ src/main/java/com/kishore/kamal/config/ApplicationContextConfig.java
@@ -3,8 +3,10 @@ package com.kishore.kamal.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.kishore.kamal.controller")
 public class ApplicationContextConfig {
